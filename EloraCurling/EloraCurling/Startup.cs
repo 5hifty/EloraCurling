1c8429d2aed9f23c5c29d4a6aeaee271fdeb9d1a
jordanmkasla2009 <jordanmkasla2009@gmail.com>
2016-02-23 19:47:24 -0500

    Create Project

---
+++ EloraCurling/EloraCurling/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EloraCurling.Startup))]
+namespace EloraCurling
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
